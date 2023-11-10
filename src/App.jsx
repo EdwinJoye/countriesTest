87774fdd28a3cc025c159f5ffb3b287b14921fc8
Edwin <edwinjoye@gmail.com>
2023-11-10 15:55:25 +0100

    css avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState, useEffect } from "react";
 import CountryCard from "./components/countryCard";
+import Moon from "../src/img/moon.png";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [value, setValue] = useState("");
+  const [search, setSearch] = useState("");
+  const [regionFilter, setRegionFilter] = useState("");
+  const [dark, setDark] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
+        let apiUrl = `https://restcountries.com/v3.1/all`;
+        if (search) {
+          apiUrl += `?fields=name,flags&name=${search}`;
+        }
+        if (regionFilter) {
+          apiUrl += `&region=${regionFilter}`;
+        }
+
+        const response = await fetch(apiUrl);
         const data = await response.json();
         setCountries(data);
       } catch (error) {
@@ -17,39 +28,66 @@ function App() {
       }
     };
     fetchData();
-  }, []);
+  }, [search, regionFilter]);
 
   return (
-    <div>
-      <section>
-        <div>Where is the world ?</div>
-        <div>Dark Mode</div>
-        <div>Light Mode</div>
+    <div className={dark ? "darkMode-color1" : "lightMode-color1"}>
+      <section
+        className={
+          dark
+            ? "darkMode-color2 app-section1"
+            : "lightMode-color2 app-section1"
+        }
+      >
+        <div className="app-section1-title">Where is the world ?</div>
+        <div>
+          <div onClick={() => setDark(!dark)} className="app-section1-modes">
+            <img src={Moon} alt="moon" />
+            <span>Dark Mode</span>
+            {console.log("DARK", dark)}
+          </div>
+          {/* <div>Light Mode</div> */}
+        </div>
       </section>
       <div className="container">
         <section className="app-section2">
           <input
+            className={dark ? "darkMode-color2" : "lightMode-color2"}
             type="text"
             placeholder="Search for a country"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
-          <div>
-            {console.log("VALUE", value)}
-            <select name="regions" id="region-select">
-              <option value="">Filter by Regions</option>
-              <option value="Africa">Africa</option>
-              <option value="America">America</option>
-              <option value="Asia">Asia</option>
-              <option value="Europe">Europe</option>
-              <option value="Oceania">Oceania</option>
-            </select>
-          </div>
+          <select
+            className={dark ? "darkMode-color2" : "lightMode-color2"}
+            name="regions"
+            id="region-select"
+            value={regionFilter}
+            onChange={(e) => setRegionFilter(e.target.value)}
+          >
+            <option value="">Filter by Regions</option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
         </section>
-        <CountryCard countries={countries} />
+        <section className="app-section3">
+          {countries.map((country) => {
+            return (
+              <CountryCard
+                flag={country.flags.png}
+                name={country.name.common}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+                dark={dark}
+              ></CountryCard>
+            );
+          })}
+        </section>
       </div>
-
-      {console.log("COUNTRY", countries)}
     </div>
   );
 }
--- src/components/countryCard.jsx
+++ src/components/countryCard.jsx
@@ -1,33 +1,32 @@
 import "../css/countryCard.css";
-const CountryCard = ({ countries }) => {
+const CountryCard = ({ flag, name, population, region, capital, dark }) => {
   return (
-    <section className="countryCard">
-      {console.log("countries444", countries)}
-      {countries.map((country) => {
-        return (
-          <div className="countryCard-container">
-            <div className="countryCard-img-container">
-              <img src={country.flags.png} alt="flag" />
-            </div>
-            <div className="countryCard-infos-container">
-              <h2>{country.name.common}</h2>
-              <div className="countryCard-info-container">
-                <span>Population:</span>
-                <div>{country.population}</div>
-              </div>
-              <div className="countryCard-info-container">
-                <span>Region:</span>
-                <div>{country.region}</div>
-              </div>
-              <div className="countryCard-info-container">
-                <span>Capital:</span>
-                <div>{country.capital}</div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </section>
+    <div className="countryCard">
+      <div className="countryCard-img-container">
+        <img src={flag} alt="flag" />
+      </div>
+      <div
+        className={
+          dark
+            ? "darkMode-color2 countryCard-infos-container"
+            : "lightMode-color2 countryCard-infos-container"
+        }
+      >
+        <h2>{name}</h2>
+        <div className="countryCard-info-container">
+          <span>Population:</span>
+          <div>{population}</div>
+        </div>
+        <div className="countryCard-info-container">
+          <span>Region:</span>
+          <div>{region}</div>
+        </div>
+        <div className="countryCard-info-container">
+          <span>Capital:</span>
+          <div>{capital}</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
